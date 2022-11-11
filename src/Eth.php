d6f6537f1dd452ded9fbb54999991c2fec0bcf22
Emir Bugra Koksalan <kodmanyagha@gmail.com>
2022-11-11 15:19:53 +0300

    last changes

--- src/Eth.php
+++ src/Eth.php
@@ -183,7 +183,7 @@ class Eth
         $txid    = '';
         $txError = null;
 
-        $tx    = new Transaction(Utils::toHex($nonce), $gasPrice, $gasLimit, $to, $value);
+        $tx    = new Transaction('0x' . Utils::toHex($nonce), $gasPrice, $gasLimit, $to, $value);
         $rawTx = '0x' . $tx->getRaw($privateKey);
 
         $self->sendRawTransaction(
